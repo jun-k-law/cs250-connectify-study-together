9f1e2c8db3512ab67140ccabe120a28ca81a24f2
TuiLaKem <167525338+TuiLaKem@users.noreply.github.com>
2026-02-25 19:34:00 -0800

    added stuff

--- my-react-app/src/test.jsx
+++ my-react-app/src/test.jsx
@@ -2,7 +2,7 @@
 import { BrowserRouter, Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 import Testt from './test2';
 import Connectify_Landing from './Landing_Page';
-
+import Login_Page from './Login';
 
 function Testing(){
     return (
@@ -10,6 +10,7 @@ function Testing(){
             <Routes>
                 <Route path="/" element={<Connectify_Landing />} />
                 <Route path="/test2" element={<Testt />} />
+                <Route path ="/login" element={<Login_Page />} />
             </Routes>
         </BrowserRouter>
     );
--- my-react-app/src/test2.jsx
+++ my-react-app/src/test2.jsx
@@ -1,12 +1,65 @@
 
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import {motion} from "motion/react"
+
 
 function Testt(){
     const nav = useNavigate();
+    const[showMenu, setShowMenu] = useState(false);
+    
     return(
-        <div>
-            <button onClick={() => nav("/")}>Back</button>
+        <>
+        {/* Navigation bar */}
+        <div id="navBar">
+            <div id = "navMenuCon">
+                <button 
+                    class="navMenu"
+                    onClick={() => setShowMenu(!showMenu)}>
+                    <i class="fa-solid fa-bars"></i>
+                </button>
+            </div>
+            <div id = "LogoCon">
+                <p id ="Logo">Connectify</p>
+            </div>
+
+
+        {/* Side Bar */}
         </div>
+        {showMenu &&  (
+            <motion.div id="sidebar"
+                initial={{x:"-100%"}}
+                animate={{x:0}}
+                transition={{type:'spring', duration:0.4, bounce:0.3}}
+            >
+                <button id="backMenu" onClick={() => setShowMenu(!showMenu)}>
+                    <i class="fa-solid fa-arrow-left"></i>
+                </button>
+                <button class = "navbtn" id = "homebtn"onClick={() => nav("/")}>Home</button>
+                <button class = "navbtn" onClick={() => nav("/")}>My Groups</button>
+                <button class = "navbtn" onClick={() => nav("/")}>Contact Us</button>
+                <button class = "navbtn" onClick={() => nav("/")}>Login</button>
+                <div id = "rowGroup">
+                    <button id = "navSettingBtn" onClick={() => nav("/")}><i class="fa-solid fa-gear"></i></button>
+                    <button id = "navSettingBtn" onClick={() => nav("/")}>Settings</button>
+                </div>
+                
+            </motion.div>
+        )}
+
+        {/* Navigation Space */}
+        <div id="navBarRel"></div>
+
+        <div>
+            
+        </div>
+        <div>
+            
+
+
+        </div>
+        
+        </>
     );
 }
 
