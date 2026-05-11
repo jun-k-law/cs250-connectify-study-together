9f9c7738a167dc8e0cd83d0e56d4912d9b06753e
Aaron-Sal <aaron.salgado785@gmail.com>
2026-05-11 16:19:00 -0700

    Update Landing_Page.jsx

--- my-react-app/src/Landing_Page.jsx
+++ my-react-app/src/Landing_Page.jsx
@@ -1,23 +1,28 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { useNavigate } from "react-router-dom";
 
 function Connectify_Landing(){
     const nav = useNavigate();
     return(
-        <div id="conHead_div">
+
+
+<div>
+        <div>
             <h1 id="conHead">Connectify</h1>
-            <p>Connect and Study together with your peers!</p>
+            <p id="welcome">Connect and Study together with your peers!</p>
                 <button id = "start" onClick={() =>nav("/login_page")}>
                     Start
                 </button>
-            <div>
-            <a href="https://docs.google.com/document/d/1VmhrWeGaSoA8Z1fFoUfnm6ds9oYxEkLkGJMdaK7Q66c/edit?usp=sharing" target="_blank"> Terms of Service</a>
-            
-            <p style={{color: 'black', font: '12px'}}>By accessing this website, you agree to be bound by these Terms of Service.</p>
-           
-         </div>
-           
+                
         </div> 
+
+        
+</div>
+        
+        
+
+        
+
     );
 }
 
