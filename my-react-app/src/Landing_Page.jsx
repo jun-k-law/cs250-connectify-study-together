d0de78b7eb34c3f746fd92e3180f7e6176fdb49e
Aaron-Sal <aaron.salgado785@gmail.com>
2026-05-11 12:16:24 -0700

    Update Landing_Page.jsx

--- my-react-app/src/Landing_Page.jsx
+++ my-react-app/src/Landing_Page.jsx
@@ -6,10 +6,17 @@ function Connectify_Landing(){
     return(
         <div id="conHead_div">
             <h1 id="conHead">Connectify</h1>
-            <p id="welcome">Connect and Study together with your peers!</p>
+            <p>Connect and Study together with your peers!</p>
                 <button id = "start" onClick={() =>nav("/login_page")}>
                     Start
                 </button>
+            <div>
+            <a href="https://docs.google.com/document/d/1VmhrWeGaSoA8Z1fFoUfnm6ds9oYxEkLkGJMdaK7Q66c/edit?usp=sharing" target="_blank"> Terms of Service</a>
+            
+            <p style={{color: 'black', font: '12px'}}>By accessing this website, you agree to be bound by these Terms of Service.</p>
+           
+         </div>
+           
         </div> 
     );
 }
